a3a068d722310240f16344373800e7044c9a2e82
Dinesh Parmar <dinesh@iaminworld.com>
2022-02-13 10:57:27 +0530

    Added web support
    Signed-off-by: Dinesh Parmar <dinesh@iaminworld.com>

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 String? Function(String? value) emailValidator() => (String? value) {
+      ///Email Validator Function which will be called by validator parameter in TextFormField
       if (value != null && value.isEmpty) {
         return "This Field is mandatory";
       }
@@ -12,6 +13,7 @@ String? Function(String? value) emailValidator() => (String? value) {
     };
 
 Dio buildDio() {
+  ///Building Dio which will help in intercepting the connections and debugging data.
   final dio = Dio();
   dio.interceptors.add(LogInterceptor(responseBody: true));
   return dio;
